fc27b1968fc8977c7f270241492915ff2eca523e
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-21 14:31:40 +0530

    [Ankit] Add: Welcome message

---
+++ AddressBookTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Using LINQ");
+        }
+    }
+}
